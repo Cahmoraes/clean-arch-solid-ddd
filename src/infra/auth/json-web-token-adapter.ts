49fe9bdd8fe8391ec015b6f9031449dd77817f94
Caique Moraes <ike.moraes93@gmail.com>
2024-11-15 17:31:02 -0300

    feat: implement JWT verification and add InvalidUserTokenError class

--- src/infra/auth/json-web-token-adapter.ts
+++ src/infra/auth/json-web-token-adapter.ts
@@ -1,14 +1,29 @@
 import { injectable } from 'inversify'
 import jwt from 'jsonwebtoken'
 
+import { type Either, left, right } from '@/domain/value-object/either'
+
 import type {
   AuthToken,
   Payload,
 } from '../../application/interfaces/auth-token'
+import { InvalidUserTokenError } from '../error/invalid-user-token-error'
 
 @injectable()
 export class JsonWebTokenAdapter implements AuthToken {
   public sign(payload: Payload, privateKey: string): string {
     return jwt.sign(payload, privateKey)
+  }
+
+  public verify(
+    token: string,
+    publicKey: string,
+  ): Either<InvalidUserTokenError, Payload> {
+    try {
+      const payload = jwt.verify(token, publicKey)
+      return right(payload)
+    } catch {
+      return left(new InvalidUserTokenError())
+    }
   }
 }
