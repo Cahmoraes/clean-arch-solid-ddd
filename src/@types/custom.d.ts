8322b87262910a53f1185f17a843c74e60e05aa4
Caique Moraes <ike.moraes93@gmail.com>
2024-12-08 10:13:34 -0300

    feat: add role property to user authentication and update integration tests

--- src/@types/custom.d.ts
+++ src/@types/custom.d.ts
@@ -1,13 +1,17 @@
 import 'fastify'
 
+import type { RoleTypes } from '@/domain/value-object/role'
+
 interface Sub {
   id: string
   email: string
+  role: RoleTypes
 }
 
 interface User {
   sub: Sub
   iat: number
+  exp: number
 }
 
 declare module 'fastify' {
