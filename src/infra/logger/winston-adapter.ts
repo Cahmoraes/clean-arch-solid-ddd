a61d1598442dac7f8bff4f7a72f47406fbe49883
Caique Moraes <ike.moraes93@gmail.com>
2024-12-08 08:56:13 -0300

    feat: enhance logging in RefreshTokenController and update logger interface for object messages

--- src/infra/logger/winston-adapter.ts
+++ src/infra/logger/winston-adapter.ts
@@ -9,29 +9,44 @@ export class WinstonAdapter implements Logger {
 
   constructor() {
     this.logger = winston.createLogger({
-      format: winston.format.combine(
-        winston.format.colorize(),
-        winston.format.simple(),
-      ),
+      format: winston.format.combine(...this.formats()),
       transports: [new winston.transports.Console()],
     })
   }
 
-  public error(instance: object, message: string): void {
-    this.logger.error(instance.constructor.name, {
-      message: `- ${message}`,
+  private formats() {
+    return [
+      winston.format.colorize(),
+      winston.format.printf(this.formatMessage),
+    ]
+  }
+
+  private formatMessage(info: winston.Logform.TransformableInfo): string {
+    const formattedMessage =
+      typeof info.message === 'object'
+        ? JSON.stringify(info.message, null, 2)
+        : info.message
+    return `${info.level}: ${info.instance || ''} ${formattedMessage}`
+  }
+
+  public error(instance: object, message: string | object): void {
+    this.logger.error({
+      instance: instance.constructor.name,
+      message: message,
     })
   }
 
-  public warn(instance: object, message: string): void {
-    this.logger.warn(instance.constructor.name, {
-      message: `- ${message}`,
+  public warn(instance: object, message: string | object): void {
+    this.logger.warn({
+      instance: instance.constructor.name,
+      message: message,
     })
   }
 
-  public info(instance: object, message: string): void {
-    this.logger.info(instance.constructor.name, {
-      message: `- ${message}`,
+  public info(instance: object, message: string | object): void {
+    this.logger.info({
+      instance: instance.constructor.name,
+      message: message,
     })
   }
 }
