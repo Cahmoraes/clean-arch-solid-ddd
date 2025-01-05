578f693c5ec3e00d3ed77f94aabee8910f7e1c88
Caique Moraes <ike.moraes93@gmail.com>
2025-01-05 17:32:12 -0300

    refactor: remove unsubscribe logic from DomainEventPublisher and clean up user creation event handling

--- src/application/use-case/create-user.usecase.ts
+++ src/application/use-case/create-user.usecase.ts
@@ -1,13 +1,9 @@
 import { inject, injectable } from 'inversify'
 import type { ValidationError } from 'zod-validation-error'
 
-import { DomainEventPublisher } from '@/domain/event/event-publisher'
-import { EVENTS } from '@/domain/event/events'
-import { UserCreatedEvent } from '@/domain/event/user-created-event'
 import { User } from '@/domain/user'
 import type { RoleTypes } from '@/domain/value-object/role'
 import { TYPES } from '@/infra/ioc/types'
-import type { Queue } from '@/infra/queue/queue'
 
 import { type Either, failure, success } from '../../domain/value-object/either'
 import { UserAlreadyExistsError } from '../error/user-already-exists-error'
@@ -34,8 +30,8 @@ export class CreateUserUseCase {
   constructor(
     @inject(TYPES.Repositories.User)
     private readonly userRepository: UserRepository,
-    @inject(TYPES.Queue)
-    private readonly queue: Queue,
+    // @inject(TYPES.Queue)
+    // private readonly queue: Queue,
   ) {}
 
   public async execute(
@@ -43,7 +39,7 @@ export class CreateUserUseCase {
   ): Promise<CreateUserOutput> {
     const userOrNull = await this.findUserByEmail(input.email)
     if (userOrNull) return failure(new UserAlreadyExistsError())
-    DomainEventPublisher.instance.subscribe(this.createDomainEventSubscriber())
+    // DomainEventPublisher.instance.subscribe(this.createDomainEventSubscriber())
     const userOrError = await this.createUser(input)
     if (userOrError.isFailure()) return failure(userOrError.value)
     await this.userRepository.save(userOrError.value)
@@ -56,17 +52,18 @@ export class CreateUserUseCase {
     return this.userRepository.findByEmail(email)
   }
 
-  private createDomainEventSubscriber() {
-    return (event: UserCreatedEvent) => {
-      this.queue.publish(
-        EVENTS.USER_CREATED,
-        new UserCreatedEvent({
-          name: event.payload.name,
-          email: event.payload.email,
-        }),
-      )
-    }
-  }
+  // DomainEventPublisher.instance.subscribe(this.createDomainEventSubscriber())
+  // private createDomainEventSubscriber() {
+  //   return (event: UserCreatedEvent) => {
+  //     this.queue.publish(
+  //       EVENTS.USER_CREATED,
+  //       new UserCreatedEvent({
+  //         name: event.payload.name,
+  //         email: event.payload.email,
+  //       }),
+  //     )
+  //   }
+  // }
 
   private async createUser(input: CreateUserUseCaseInput) {
     return User.create({
