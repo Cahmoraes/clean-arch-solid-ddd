30775a8d479fddeab4d9c865466147e3ed5d12d0
Caique Moraes <ike.moraes93@gmail.com>
2025-01-06 13:39:28 -0300

    feat: integrate user creation with domain event publishing and queue handling

--- src/application/use-case/create-user.usecase.ts
+++ src/application/use-case/create-user.usecase.ts
@@ -1,9 +1,13 @@
 import { inject, injectable } from 'inversify'
 import type { ValidationError } from 'zod-validation-error'
 
+import { DomainEventPublisher } from '@/domain/event/event-publisher'
+import { EVENTS } from '@/domain/event/events'
+import { UserCreatedEvent } from '@/domain/event/user-created-event'
 import { User } from '@/domain/user'
 import type { RoleTypes } from '@/domain/value-object/role'
 import { TYPES } from '@/infra/ioc/types'
+import type { Queue } from '@/infra/queue/queue'
 
 import { type Either, failure, success } from '../../domain/value-object/either'
 import { UserAlreadyExistsError } from '../error/user-already-exists-error'
@@ -30,13 +34,23 @@ export class CreateUserUseCase {
   constructor(
     @inject(TYPES.Repositories.User)
     private readonly userRepository: UserRepository,
-  ) {}
+    @inject(TYPES.Queue)
+    private readonly queue: Queue,
+  ) {
+    this.bindMethod()
+  }
+
+  private bindMethod(): void {
+    this.createDomainEventSubscriber =
+      this.createDomainEventSubscriber.bind(this)
+  }
 
   public async execute(
     input: CreateUserUseCaseInput,
   ): Promise<CreateUserOutput> {
     const userOrNull = await this.userOfEmail(input.email)
     if (userOrNull) return failure(new UserAlreadyExistsError())
+    DomainEventPublisher.instance.subscribe(this.createDomainEventSubscriber)
     const userOrError = await this.createUser(input)
     if (userOrError.isFailure()) return failure(userOrError.value)
     await this.userRepository.save(userOrError.value)
@@ -47,6 +61,16 @@ export class CreateUserUseCase {
 
   private async userOfEmail(email: string): Promise<User | null> {
     return this.userRepository.userOfEmail(email)
+  }
+
+  private createDomainEventSubscriber(event: UserCreatedEvent) {
+    this.queue.publish(
+      EVENTS.USER_CREATED,
+      new UserCreatedEvent({
+        name: event.payload.name,
+        email: event.payload.email,
+      }),
+    )
   }
 
   private async createUser(input: CreateUserUseCaseInput) {
