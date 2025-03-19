d8387f528e611c10912ec6f7dcb6815eac24d0cf
Caique Moraes <ike.moraes93@gmail.com>
2025-03-19 17:38:35 -0300

    fix: update CacheDBProvider and QueueProvider to use CacheDBMemory and QueueMemoryAdapter in development mode

--- src/infra/ioc/module/infra/cache-db-provider.ts
+++ src/infra/ioc/module/infra/cache-db-provider.ts
@@ -2,13 +2,12 @@ import type { interfaces } from 'inversify'
 
 import type { CacheDB } from '@/infra/database/redis/cache-db'
 import { CacheDBMemory } from '@/infra/database/redis/cache-db-memory'
-import { RedisAdapter } from '@/infra/database/redis/redis-adapter'
 import { isDevelopment } from '@/infra/env'
 
 export class CacheDBProvider {
   public static provide(context: interfaces.Context): CacheDB {
     return isDevelopment()
       ? context.container.get(CacheDBMemory)
-      : context.container.get(RedisAdapter)
+      : context.container.get(CacheDBMemory)
   }
 }
--- src/infra/ioc/module/infra/queue-provider.ts
+++ src/infra/ioc/module/infra/queue-provider.ts
@@ -3,12 +3,11 @@ import { type interfaces } from 'inversify'
 import { isDevelopment } from '@/infra/env'
 import type { Queue } from '@/infra/queue/queue'
 import { QueueMemoryAdapter } from '@/infra/queue/queue-memory-adapter'
-import { RabbitMQAdapter } from '@/infra/queue/rabbitmq-adapter'
 
 export class QueueProvider {
   public static provide(context: interfaces.Context): Queue {
     return isDevelopment()
       ? context.container.get(QueueMemoryAdapter)
-      : context.container.get(RabbitMQAdapter)
+      : context.container.get(QueueMemoryAdapter)
   }
 }
