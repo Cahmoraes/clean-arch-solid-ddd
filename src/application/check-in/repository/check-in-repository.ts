652915db45618982c261cfe7aa6445698ada7e75
Caique Moraes <ike.moraes93@gmail.com>
2025-05-26 09:02:31 -0300

    feat: add check-in creation endpoint and implement transaction management in repositories

--- src/application/check-in/repository/check-in-repository.ts
+++ src/application/check-in/repository/check-in-repository.ts
@@ -10,4 +10,5 @@ export interface CheckInRepository {
   onSameDateOfUserId(userId: string, date: Date): Promise<boolean>
   checkInsOfUserId(userId: string, page: number): Promise<CheckIn[]>
   countOfUserId(userId: string): Promise<number>
+  withTransaction(object: unknown): CheckInRepository
 }
