f076da01a88a778aa084ee0cbb04eb8982237299
Caique Moraes <ike.moraes93@gmail.com>
2025-06-01 12:09:47 -0300

    feat: integrate NodeCache for in-memory caching and enhance Redis adapter with fallback mechanism
    - Added NodeCache dependency and implemented in-memory caching in CacheDBMemory.
    - Updated RedisAdapter to include fallback to in-memory cache when Redis is unavailable.
    - Enhanced error handling and reconnection logic for Redis.
    - Modified CacheDBProvider to use RedisAdapter for both production and non-production environments.
    - Cleaned up unused console logs and ensured consistent logging for Redis operations.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,18 @@
 import pluginJs from '@eslint/js'
+import importPlugin from 'eslint-plugin-import'
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
 import simpleImportSort from 'eslint-plugin-simple-import-sort'
 import globals from 'globals'
 import tseslint from 'typescript-eslint'
 
 export default [
-  { files: ['**/*.{js,mjs,cjs,ts}'] },
+  {
+    files: ['**/*.{js,mjs,cjs,ts}'],
+  },
+  importPlugin.flatConfigs.recommended,
+  importPlugin.flatConfigs.typescript,
   { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+
   {
     plugins: {
       'simple-import-sort': simpleImportSort,
@@ -16,6 +22,17 @@ export default [
   ...tseslint.configs.recommended,
   eslintPluginPrettierRecommended,
   {
+    settings: {
+      'import/resolver': {
+        typescript: {
+          alwaysTryTypes: true,
+          project: './tsconfig.json',
+        },
+        node: {
+          extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        },
+      },
+    },
     rules: {
       complexity: ['error', 5],
       'simple-import-sort/imports': 'error',
@@ -29,6 +46,24 @@ export default [
     files: ['**/*.test.ts'],
     rules: {
       complexity: 'off', // Desativa a regra de complexidade para testes
+    },
+  },
+  {
+    rules: {
+      'import/no-unresolved': 'off',
+      'import/no-restricted-paths': [
+        'error',
+        {
+          zones: [
+            {
+              target: './src/domain',
+              from: './src/application',
+              message:
+                'A camada domain não pode importar da camada application',
+            },
+          ],
+        },
+      ],
     },
   },
 ]
--- src/infra/database/redis/cache-db-memory.ts
+++ src/infra/database/redis/cache-db-memory.ts
@@ -1,21 +1,30 @@
+import { injectable } from 'inversify'
+import NodeCache from 'node-cache'
+
 import type { CacheDB } from './cache-db'
 
+@injectable()
 export class CacheDBMemory implements CacheDB {
   public data: Map<string, any> = new Map()
+  private _cache: NodeCache
+
+  constructor() {
+    this._cache = new NodeCache()
+  }
 
   public async get<T>(key: string): Promise<T | null> {
-    return this.data.get(key) ?? null
+    return this._cache.get(key) ?? null
   }
 
   public async set<T>(key: string, value: T): Promise<void> {
-    this.data.set(key, value)
+    this._cache.set(key, value)
   }
 
   public async delete(key: string): Promise<void> {
-    this.data.delete(key)
+    this._cache.del(key)
   }
 
   public async clear(): Promise<void> {
-    this.data.clear()
+    this._cache.flushAll()
   }
 }
