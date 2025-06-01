1556e85c2e90e6e61d7c8e026d77dbb06d090b44
Caique Moraes <ike.moraes93@gmail.com>
2025-06-01 12:09:47 -0300

    feat: integrate NodeCache for in-memory caching and enhance Redis adapter with fallback mechanism
    - Added NodeCache dependency and implemented in-memory caching in CacheDBMemory.
    - Updated RedisAdapter to include fallback to in-memory cache when Redis is unavailable.
    - Enhanced error handling and reconnection logic for Redis.
    - Modified CacheDBProvider to use RedisAdapter for both production and non-production environments.
    - Cleaned up unused console logs and ensured consistent logging for Redis operations.

--- src/infra/ioc/module/infra/cache-db-provider.ts
+++ src/infra/ioc/module/infra/cache-db-provider.ts
@@ -1,13 +1,14 @@
 import type { ResolutionContext } from 'inversify'
 
 import type { CacheDB } from '@/infra/database/redis/cache-db'
-import { CacheDBMemory } from '@/infra/database/redis/cache-db-memory'
+// import { CacheDBMemory } from '@/infra/database/redis/cache-db-memory'
+import { RedisAdapter } from '@/infra/database/redis/redis-adapter'
 import { isProduction } from '@/infra/env'
 
 export class CacheDBProvider {
   public static provide(context: ResolutionContext): CacheDB {
     return isProduction()
-      ? context.get(CacheDBMemory)
-      : context.get(CacheDBMemory)
+      ? context.get(RedisAdapter)
+      : context.get(RedisAdapter)
   }
 }
