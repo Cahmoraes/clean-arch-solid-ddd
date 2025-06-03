fad1313dac07954112d267f4fbc9497f32189002
Caique Moraes <ike.moraes93@gmail.com>
2025-06-03 20:40:43 -0300

    feat: adicionar verificação de instância inválida no método withTransaction e refatorar a interface CheckInRepository

---
+++ src/infra/errors/invalid-transaction-instance-error.ts
@@ -0,0 +1,7 @@
+export class InvalidTransactionInstance extends Error {
+  constructor(object: object) {
+    const errorConstructor = Object.getPrototypeOf(object)
+    super(`Invalid transaction instance provided: ${errorConstructor.name}`)
+    this.name = 'InvalidTransactionInstance'
+  }
+}
