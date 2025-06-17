e8180f09e5850595845683eb0c7b9d92f56943ca
Caique Moraes <ike.moraes93@gmail.com>
2025-06-17 12:10:13 -0300

    feat: adiciona caso de uso e testes para logout de usuário, atualiza contêiner de injeção de dependências

--- src/shared/infra/ioc/module/session/session-container.ts
+++ src/shared/infra/ioc/module/session/session-container.ts
@@ -1,11 +1,13 @@
 import { ContainerModule } from 'inversify'
 
-import { AuthenticateController } from '@/session/infra/controller/authenticate.controller'
 import { AuthenticateUseCase } from '@/session/application/use-case/authenticate.usecase'
+import { LogoutUseCase } from '@/session/application/use-case/logout.usecase'
+import { AuthenticateController } from '@/session/infra/controller/authenticate.controller'
 
 import { TYPES } from '../../types'
 
 export const sessionContainer = new ContainerModule(({ bind }) => {
   bind(TYPES.Controllers.Authenticate).to(AuthenticateController)
   bind(TYPES.UseCases.Authenticate).to(AuthenticateUseCase)
+  bind(TYPES.UseCases.Logout).to(LogoutUseCase)
 })
