82e56a61f0b52afc61678985d94aa2753d80666b
Caique Moraes <ike.moraes93@gmail.com>
2025-06-21 22:36:26 -0300

    feat: adiciona provedores de verificação de saúde para banco de dados e cache

--- src/shared/infra/ioc/module/health-check/heath-check-container.ts
+++ src/shared/infra/ioc/module/health-check/heath-check-container.ts
@@ -1,9 +1,11 @@
 import { ContainerModule } from 'inversify'
 
-import { DatabaseHealthProvider } from '@/shared/infra/health/database-health-provider'
+import { CacheHealthProvider } from '@/shared/infra/health/cache/cache-health-provider'
+import { DatabaseHealthProvider } from '@/shared/infra/health/database/database-health-provider'
 
 import { TYPES } from '../../types'
 
 export const healthCheckContainer = new ContainerModule(({ bind }) => {
   bind(TYPES.HealthCheck.Database).to(DatabaseHealthProvider)
+  bind(TYPES.HealthCheck.Cache).to(CacheHealthProvider)
 })
