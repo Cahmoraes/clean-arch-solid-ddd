a060f6e3f621def26ac33d1b4102c9666324e2a2
Caique Moraes <ike.moraes93@gmail.com>
2025-06-23 18:24:36 -0300

    feat: adiciona controlador e rotas para verificação de saúde

--- src/shared/infra/controller/health-check-controller.ts
+++ src/shared/infra/controller/health-check-controller.ts
@@ -2,16 +2,36 @@ import { inject, injectable } from 'inversify'
 
 import type { HealthCheckService } from '../health/health-check.service'
 import { TYPES } from '../ioc/types'
+import type { HttpServer } from '../server/http-server'
 import type { Controller } from './controller'
+import { ResponseFactory } from './factory/response-factory'
+import { HealthCheckRoutes } from './routes/health-check-routes'
 
 @injectable()
 export class HealthCheckController implements Controller {
   constructor(
+    @inject(TYPES.Server.Fastify)
+    private readonly httpServer: HttpServer,
     @inject(TYPES.HealthCheck.Service)
     private readonly healthCheckService: HealthCheckService,
-  ) {}
+  ) {
+    this.bindMethods()
+  }
+
+  private bindMethods(): void {
+    this.callback = this.callback.bind(this)
+  }
 
   public async init(): Promise<void> {
-    throw new Error('Method not implemented.')
+    this.httpServer.register('get', HealthCheckRoutes.check, {
+      callback: this.callback,
+    })
+  }
+
+  private async callback() {
+    const healthStatus = await this.healthCheckService.check()
+    return ResponseFactory.OK({
+      healthStatus,
+    })
   }
 }
