f22e04b0355859ba077a878c91bb7a921613aee3
Caique Moraes <ike.moraes93@gmail.com>
2025-06-28 17:56:22 -0300

    feat: adiciona logger e interfaces para dados de usuários no cache

--- src/shared/infra/cron/task/update-user-profile-cache-task.ts
+++ src/shared/infra/cron/task/update-user-profile-cache-task.ts
@@ -1,11 +1,27 @@
 import { inject, injectable } from 'inversify'
 
 import type { FetchUsersOutput, UserDAO } from '@/user/application/dao/user-dao'
+import type { RoleTypes } from '@/user/domain/value-object/role'
 
 import type { CacheDB } from '../../database/redis/cache-db'
 import { env } from '../../env'
 import { TYPES } from '../../ioc/types'
+import type { Logger } from '../../logger/logger'
 import type { Task } from './task'
+
+export interface UsersData {
+  id: string
+  role: RoleTypes
+  createdAt: string
+  name: string
+  email: string
+}
+
+export interface CacheUsersData {
+  data: {
+    usersData: UsersData[]
+  }
+}
 
 @injectable()
 export class UpdateUserProfileCacheTask implements Task {
@@ -14,6 +30,8 @@ export class UpdateUserProfileCacheTask implements Task {
     private readonly userDAO: UserDAO,
     @inject(TYPES.Redis)
     private readonly cacheDB: CacheDB,
+    @inject(TYPES.Logger)
+    private readonly logger: Logger,
   ) {
     this.bindMethod()
   }
@@ -28,9 +46,13 @@ export class UpdateUserProfileCacheTask implements Task {
       page: 1,
     })
     await this.saveUserDataToCache(usersData)
-    const cacheResult = await this.cacheDB.get('fetch-users:1:10')
-    console.log({ usersData })
-    console.log({ cacheResult })
+    const cacheResult =
+      await this.cacheDB.get<CacheUsersData>('fetch-users:1:10')
+    this.logger.info(this, `User profile cache updated`)
+    this.logger.info(
+      this,
+      `Cache data: ${JSON.stringify(cacheResult!.data.usersData[0], null, 2)}`,
+    )
   }
 
   private async saveUserDataToCache(
