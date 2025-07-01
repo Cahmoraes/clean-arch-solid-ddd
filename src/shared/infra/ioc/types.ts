63e8398e7570d04d7fe786a3d059d4b0a7374712
Caique Moraes <ike.moraes93@gmail.com>
2025-07-01 14:50:29 -0300

    feat: adiciona novos tipos de serviço e reorganiza exportações de tipos

--- src/shared/infra/ioc/types.ts
+++ src/shared/infra/ioc/types.ts
@@ -1,7 +1,7 @@
 // Barrel export - Re-exports all type modules for easy importing
-export { AUTH_TYPES } from './auth-types'
-export { CHECKIN_TYPES } from './checkin-types'
-export { GYM_TYPES } from './gym-types'
-export { HEALTH_CHECK_TYPES } from './health-check-types'
-export { SHARED_TYPES } from './shared-types'
-export { USER_TYPES } from './user-types'
+export { AUTH_TYPES } from './module/service-identifier/auth-types'
+export { CHECKIN_TYPES } from './module/service-identifier/checkin-types'
+export { GYM_TYPES } from './module/service-identifier/gym-types'
+export { HEALTH_CHECK_TYPES } from './module/service-identifier/health-check-types'
+export { SHARED_TYPES } from './module/service-identifier/shared-types'
+export { USER_TYPES } from './module/service-identifier/user-types'
