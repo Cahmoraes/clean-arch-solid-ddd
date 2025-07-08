ff5f297653f3dcda8af84c494d238cef447f023c
Caique Moraes <ike.moraes93@gmail.com>
2025-07-07 22:11:29 -0300

    refactor: sessionDAO to revoked token management with DAO pattern and update related components

---
+++ src/shared/infra/database/dao/in-memory/revoked-token-dao-memory.ts
@@ -0,0 +1,24 @@
+import ExtendedSet from '@cahmoraes93/extended-set'
+import { injectable } from 'inversify'
+
+import type {
+  RevokedTokenDAO,
+  RevokedTokenData,
+} from '@/session/application/dao/revoked-token-dao'
+
+@injectable()
+export class RevokedTokenDAOMemory implements RevokedTokenDAO {
+  public revokedTokenData = new ExtendedSet<RevokedTokenData>()
+
+  public async revoke(session: RevokedTokenData): Promise<void> {
+    this.revokedTokenData.add(session)
+  }
+
+  public async revokedTokenById(id: string): Promise<RevokedTokenData | null> {
+    return this.revokedTokenData.find((session) => id === session.jwi)
+  }
+
+  public async delete(session: RevokedTokenData): Promise<void> {
+    this.revokedTokenData.delete(session)
+  }
+}
