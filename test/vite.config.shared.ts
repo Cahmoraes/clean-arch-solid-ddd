2a065a6e5234742134f90c41c1d2098bf50755af
Caique Moraes <ike.moraes93@gmail.com>
2026-01-14 10:32:46 -0300

    fix: corrigir caminho do arquivo de configuração de testes

--- test/vite.config.shared.ts
+++ test/vite.config.shared.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "vitest/config"
 export default defineConfig({
 	plugins: [tsconfigPaths()],
 	test: {
-		setupFiles: "/test/setup-test.ts",
+		setupFiles: "./test/setup-test.ts",
 		globals: true,
 		sequence: {
 			concurrent: false,
