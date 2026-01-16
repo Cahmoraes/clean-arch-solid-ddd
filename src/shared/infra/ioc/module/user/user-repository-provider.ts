943fb47864a70a724921efe71219802d0f928f22
Caique Moraes <ike.moraes93@gmail.com>
2026-01-16 15:10:39 -0300

    feat: adicionar suporte ao SQLite como provedor de banco de dados e atualizar repositórios

--- src/shared/infra/ioc/module/user/user-repository-provider.ts
+++ src/shared/infra/ioc/module/user/user-repository-provider.ts
@@ -2,13 +2,25 @@ import type { ResolutionContext } from "inversify"
 
 import { InMemoryUserRepository } from "@/shared/infra/database/repository/in-memory/in-memory-user-repository.js"
 import { PrismaUserRepository } from "@/shared/infra/database/repository/prisma/prisma-user-repository"
-import { isProduction } from "@/shared/infra/env"
+import { SQLiteUserRepository } from "@/shared/infra/database/repository/sqlite/sqlite-user-repository"
+import { env, isProduction } from "@/shared/infra/env"
 import type { UserRepository } from "@/user/application/persistence/repository/user-repository"
 
 export class UserRepositoryProvider {
 	public static provide(context: ResolutionContext): UserRepository {
 		return isProduction()
-			? context.get(PrismaUserRepository, { autobind: true })
+			? context.get(UserRepositoryProvider.selectDatabaseByProvider(), {
+					autobind: true,
+				})
 			: context.get(InMemoryUserRepository, { autobind: true })
+	}
+
+	private static selectDatabaseByProvider() {
+		switch (env.DATABASE_PROVIDER) {
+			case "prisma":
+				return PrismaUserRepository
+			default:
+				return SQLiteUserRepository
+		}
 	}
 }
